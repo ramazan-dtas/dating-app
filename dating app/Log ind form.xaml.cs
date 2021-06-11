90dc25773eea16ec6dd0e7ad8c4e53fb019bce49
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2021-06-11 10:17:43 +0200

    first

---
+++ dating app/Log ind form.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dating_app
+{
+    /// <summary>
+    /// Interaction logic for Log_ind_form.xaml
+    /// </summary>
+    public partial class Log_ind_form : Window
+    {
+        public Log_ind_form()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Konto oprettet!!!");
+            MainWindow main = new MainWindow();
+            main.Show();
+            
+        }
+
+       
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).Text = "";
+        }
+
+        
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow tilbage = new MainWindow();
+            tilbage.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ dating app/OpretKonto.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dating_app
+{
+    /// <summary>
+    /// Interaction logic for OpretKonto.xaml
+    /// </summary>
+    public partial class OpretKonto : Window
+    {
+        public OpretKonto()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Du er nu logget ind");
+            MainWindow tilbage = new MainWindow();
+            tilbage.Show();
+            this.Hide();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow tilbage = new MainWindow();
+            tilbage.Show();
+            this.Hide();
+        }
+    }
+}
